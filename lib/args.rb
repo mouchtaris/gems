68773b2894ad583b1bd818c117e21f4bfc6b5ded
Nikos <n.mouchtaris@youngcapital.nl>
2019-03-14 10:09:02 +0000

    Add Array[] and Hash[]

--- lib/args.rb
+++ lib/args.rb
@@ -8,7 +8,7 @@ module Args
     class << self
       alias_method :can_be_from?, :===
       def optionally_create(name, value, check)
-        return unless can_be_from? value
+        return unless can_be_from? check
         new(name, value, check)
       end
     end
@@ -21,14 +21,14 @@ module Args
       @name = name
       @value = value
       @check = check
-      msg = "#{error_message}\n#{value.inspect}"
-      super(msg)
+      #msg = "#{error_message}\n#{value.inspect}"
+      super()#(msg)
     end
   end
 
   class IsNotType < Check
-    def self.can_be_from?(arg)
-      arg.is_a? Module
+    def self.can_be_from?(check)
+      check.is_a? Module
     end
 
     def check_value
@@ -42,8 +42,51 @@ module Args
     end
   end
 
+  class IsNotArrayOf < Check
+    include Args
+
+    def self.can_be_from?(check)
+      check.is_a? Array
+    end
+
+    def check_value
+      @value.tap(&check(@name, Array))
+      @value.each_with_index do |element, index|
+        name = "#{@name}-#{index}"
+        element.tap(&check(name, @check.first))
+      end
+    end
+
+    def error_message
+      'Not an array of'
+    end
+  end
+
+  class IsNotStructOf < Check
+    include Args
+
+    def self.can_be_from?(check)
+      check.is_a? Hash
+    end
+
+    def check_value
+      @value.tap(&check(@name, Hash))
+      @check.each do |key, subcheck|
+        name = "#{@name}/#{key}"
+        value = @value[key] || @value[key.to_s]
+        value.tap(&check(name, subcheck))
+      end
+    end
+
+    def error_message
+      'Not a hash of'
+    end
+  end
+
   Checks = [
-    IsNotType
+    IsNotType,
+    IsNotArrayOf,
+    IsNotStructOf,
   ].freeze
 
   def check(name, check)
