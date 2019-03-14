50a168522faedb025563e5ec2597cb4354b43953
Nikos <n.mouchtaris@youngcapital.nl>
2019-03-14 09:23:58 +0000

    Add implementation

--- lib/args.rb
+++ lib/args.rb
@@ -1,6 +1,63 @@
-require "args/version"
+require 'args/version'
 
 module Args
-  class Error < StandardError; end
-  # Your code goes here...
+  class Error < ArgumentError
+  end
+
+  class Check < Error
+    class << self
+      alias_method :can_be_from?, :===
+      def optionally_create(name, value, check)
+        return unless can_be_from? value
+        new(name, value, check)
+      end
+    end
+
+    def check_or_raise!
+      check_value || (raise self)
+    end
+
+    def initialize(name, value, check)
+      @name = name
+      @value = value
+      @check = check
+      msg = "#{error_message}\n#{value.inspect}"
+      super(msg)
+    end
+  end
+
+  class IsNotType < Check
+    def self.can_be_from?(arg)
+      arg.is_a? Module
+    end
+
+    def check_value
+      @value.is_a? @check
+    end
+
+    def error_message
+      clazz = @value.class
+      ancestors = clazz.ancestors.join(', ')
+      "#{@name}:#{clazz}[#{ancestors}] !>= [#{@check}]"
+    end
+  end
+
+  Checks = [
+    IsNotType
+  ].freeze
+
+  def check(name, check)
+    -> (value) {
+      Checks.each do |c|
+        c
+          .optionally_create(name, value, check)
+          &.check_or_raise!
+      end
+    }
+  end
+
+  def get(params, name, check)
+    params[name.to_s]
+      .tap(&check(name, check))
+  end
 end
