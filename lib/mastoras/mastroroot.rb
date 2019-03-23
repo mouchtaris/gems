c27a0da84405d598dcd8f4a8e0ad79fdaf535c48
Nikos Mouchtaris <n.mouchtaris@youngcapital.nl>
2019-03-23 10:38:21 +0000

    Add mastoras/mastroroot

---
+++ lib/mastoras/mastroroot.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'args'
+
+module Mastoras
+  class Mastroroot
+    include Args
+
+    MASTROFILE_NAME = 'mastroroot'
+
+    def initialize(mastroroot)
+      @root = mastroroot
+        .tap(&check(:mastroroot, Pathname))
+    end
+
+    def mastrofile
+      @root / MASTROFILE_NAME
+    end
+  end
+end
