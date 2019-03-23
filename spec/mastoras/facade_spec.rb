14d351835597504fde8741d89e12cd329d6f43c1
Nikos Mouchtaris <n.mouchtaris@youngcapital.nl>
2019-03-23 09:15:54 +0000

    Structify facade specs

--- spec/mastoras/facade_spec.rb
+++ spec/mastoras/facade_spec.rb
@@ -8,13 +8,17 @@ RSpec.describe Mastoras::Facade do
   it 'exists' do
   end
 
-  it 'is constructible with a mastoroot Pathname' do
-    expect { described_class.new Pathname.new '/somewhere/' }
-      .not_to raise_error
-  end
+  describe '#initialize (construction)' do
+    subject(:construction) { described_class.new(*construction_arguments) }
 
-  it 'does not accept non-path-name roots' do
-    expect { described_class.new '/somewhere/' }
-      .to raise_error ArgumentError
+    describe 'with a Pathname' do
+      let(:construction_arguments) { [Pathname.new('/somewhere/')] }
+      it { expect { subject }.not_to raise_error }
+    end
+
+    describe 'with a String' do
+      let(:construction_arguments) { ['/somewhere/'] }
+      it { expect { subject }.to raise_error ArgumentError }
+    end
   end
 end
