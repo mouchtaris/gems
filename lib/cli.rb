8e6314003f391ab3f567d7f80fca356f894c582c
Nikos Mouchtaris <n.mouchtaris@youngcapital.nl>
2019-03-26 15:25:58 +0000

    Parsing opts

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,19 +1,54 @@
+#!/usr/bin/env ruby
+require 'optparse'
+
+require 'bundler/setup'
+
 module Cli
   module_function
 
-  def option_parser
-    @option_parser ||= ::OptParse.new.tap do |opt|
+  class Options
+    SetupParser = lambda do |prs|
+      prs.instance_exec do
+        on '-h', '--help', 'Print this message'
+      end
+    end
+
+    def initialize(params)
+      @params = params.dup.freeze
+    end
+
+    def parser
+      @parser ||= OptionParser.new.tap(&SetupParser)
+    end
+
+    def to_s
+      parser.to_s
+    end
+
+    def parse
+      @parse ||= (
+        opts = {}
+        args = parser.parse(@params, into: opts)
+        [opts, args]
+      )
+    end
+
+    def opts
+      @opts ||= parse[0]
+    end
+
+    def args
+      @args ||= parse[1]
+    end
+
+    def help?
+      @help ||= (opts[:help] == true)
     end
   end
 
   def cli(args)
-    require_relative 'cyr'
-    params = {}
-    opt = option_parser
-    opt.parse(args, into: params)
-    puts opt
-    pp params
-    pp args
+    opts = Options.new(args)
+    return puts opts if opts.help?
   end
 end
 
