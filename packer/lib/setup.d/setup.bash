9c8c67998806d6847918ea555e2eec1cfb6b604e
Nikos Mouchtaris <n.mouchtaris@youngcapital.nl>
2019-03-30 17:41:00 +0000

    Do not run setup.bash recursively

--- packer/lib/setup.d/setup.bash
+++ packer/lib/setup.d/setup.bash
@@ -3,4 +3,12 @@ find /mastoras/setup.d \
   -maxdepth 1 \
   -name '*.bash' \
 | sort \
-| xargs bash -c 'for arg; do echo "*** $arg"; bash "$arg" | sed -r -e s,^,"[$arg] ",; done' --
+| xargs bash -c '
+  for arg;
+  do
+    if [ test "$arg" != "setup.bash" ]
+    then
+      echo "*** $arg"
+      bash "$arg" | sed -r -e "s,^,[$arg] ,"
+    fi
+  done' --
