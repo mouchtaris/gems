936c29dbfb0beb213cb1a562b26ab374b6210474
Nikos Mouchtaris <n.mouchtaris@youngcapital.nl>
2019-04-01 08:27:21 +0000

    Mastrinit

---
+++ packer/lib/mastrinit.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+mastoras=/mastoras &&
+sudo mkdir -pv "$mastoras" &&
+sudo chmod 777 "$mastoras" &&
+current=/mastoras/"$1" &&
+if test "$current" = "$mastoras/"
+then
+  echo 'artifact_name not given as $1' 2>&1
+  exit 1
+else true
+fi &&
+sudo rm -rfv "$current" &&
+sudo mkdir -pv "$current" &&
+sudo ln -sv "$current" /mastoras/current &&
+true
---
+++ packer/repo/ubuntu-18.04/mastrinit.bash
@@ -0,0 +1,1 @@
+../../lib/mastrinit.bash
