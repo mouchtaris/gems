b2b281dd0d7ab44c4dcd9e30e7209ff37cf0b5ef
Nikos Mouchtaris <n.mouchtaris@youngcapital.nl>
2019-04-08 15:14:59 +0000

    Fix order or commands

--- packer/repo/chefws/setup.d/300-install_files.sh
+++ packer/repo/chefws/setup.d/300-install_files.sh
@@ -1,6 +1,14 @@
 lib="$CURRENT/setup.d/lib"
 chef_d=$HOME/.chef
 chef_config=$chef_d/config.rb
+
+mkdir -pv $chef_d &&
+  cp -av $lib/config.rb $chef_config &&
+
+sudo mkdir -pv /mloc &&
+  sudo chmod 777 /mloc &&
+  cp -av $lib/get_pem_keys.bash /mloc &&
+
 cookbooks="$(ruby -- $chef_config <<-'RUBY'
 def log_level; end
 def log_location; end
@@ -13,14 +21,7 @@ def cookbook_path(cbp); @cookbook_path = cbp end
 require ARGV[0]
 puts @cookbook_path.first
 RUBY
-)"
-
-mkdir -pv $chef_d &&
-  cp -av $lib/config.rb $chef_config &&
-
-sudo mkdir -pv /mloc &&
-  sudo chmod 777 /mloc &&
-  cp -av $lib/get_pem_keys.bash /mloc &&
+)" &&
 
 mkdir -pv "$cookbooks" &&
   cd "$cookbooks" &&
