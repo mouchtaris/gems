5824a326b7e7087aa92a6769024a231400ea9fdc
Nikos Mouchtaris <n.mouchtaris@youngcapital.nl>
2019-04-08 15:48:58 +0000

    Fix file provision again

--- packer/repo/chefws/setup.d/300-install_files.sh
+++ packer/repo/chefws/setup.d/300-install_files.sh
@@ -9,14 +9,14 @@ sudo mkdir -pv /mloc &&
   sudo chmod 777 /mloc &&
   cp -av $lib/get_pem_keys.sh /mloc &&
 
-cookbooks="$(ruby -- $chef_config <<-'RUBY'
-def log_level; end
-def log_location; end
-def node_name; end
-def client_key; end
-def validation_client_name; end
-def validation_key; end
-def chef_server_url; end
+cookbooks="$(ruby - $chef_config <<-'RUBY'
+def log_level(*); end
+def log_location(*); end
+def node_name(*); end
+def client_key(*); end
+def validation_client_name(*); end
+def validation_key(*); end
+def chef_server_url(*); end
 def cookbook_path(cbp); @cookbook_path = cbp end
 require ARGV[0]
 puts @cookbook_path.first
