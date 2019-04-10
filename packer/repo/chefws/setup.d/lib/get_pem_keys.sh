d4b155b67e93b399a5c3d9b9a112e9cac0049396
Nikos Mouchtaris <n.mouchtaris@youngcapital.nl>
2019-04-10 17:55:13 +0000

    Flaxen scping ssh checks

--- packer/repo/chefws/setup.d/lib/get_pem_keys.sh
+++ packer/repo/chefws/setup.d/lib/get_pem_keys.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 [ ! -d "$HOME/.chef" ] && mkdir -pv "$HOME/.chef"
-scp server.sp.org:/mloc/crabs.pem "$HOME/.chef"
-scp server.sp.org:/mloc/spongers.pem "$HOME/.chef"
+scp 
+  -o PasswordAuthentication=no                \
+  -o StrictHostKeyChecking=no                 \
+  -o UserKnownHostsFile=/dev/null             \
+  server.sp.org:/mloc/crabs.pem "$HOME/.chef"
+
+scp 
+  -o PasswordAuthentication=no                \
+  -o StrictHostKeyChecking=no                 \
+  -o UserKnownHostsFile=/dev/null             \
+  server.sp.org:/mloc/spongers.pem "$HOME/.chef"
