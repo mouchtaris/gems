4a40220824987c9776a63808d60cdf046ae0c491
Nikos Mouchtaris <n.mouchtaris@youngcapital.nl>
2019-04-10 17:59:31 +0000

    Fix error in get_pem_keys

--- packer/repo/chefws/setup.d/lib/get_pem_keys.sh
+++ packer/repo/chefws/setup.d/lib/get_pem_keys.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 [ ! -d "$HOME/.chef" ] && mkdir -pv "$HOME/.chef"
-scp 
+scp                                           \
   -o PasswordAuthentication=no                \
   -o StrictHostKeyChecking=no                 \
   -o UserKnownHostsFile=/dev/null             \
   server.sp.org:/mloc/crabs.pem "$HOME/.chef"
 
-scp 
+scp                                           \
   -o PasswordAuthentication=no                \
   -o StrictHostKeyChecking=no                 \
   -o UserKnownHostsFile=/dev/null             \
