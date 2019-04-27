c99e262409754567f82e372e01a6804179fe2039
Nikos Mouchtaris <n.mouchtaris@youngcapital.nl>
2019-04-27 16:04:33 +0000

    Kill most str

--- spec/u/str_spec.cpp
+++ spec/u/str_spec.cpp
@@ -6,88 +6,87 @@
 #include <iostream>
 namespace
 {
-    using namespace ::u::str;
-    using stdx::is_detected;
-    using std::negation;
-
-    //
-    // char_ptr_t
-    //
-    template <
-        typename char_t = char,
-        typename fail = std::false_type
-    >
-    using char_ptr_t_detected = std::conjunction<
-        negation<fail>,
-        is_detected<char_ptr_t, char_t const* const &&>,
-        is_detected<char_ptr_t, char_t const* const &>,
-        is_detected<char_ptr_t, char_t const* const >,
-        is_detected<char_ptr_t, char_t const* &&>,
-        is_detected<char_ptr_t, char_t const* &>,
-        is_detected<char_ptr_t, char_t const* >,
-        is_detected<char_ptr_t, char_t * const &&>,
-        is_detected<char_ptr_t, char_t * const &>,
-        is_detected<char_ptr_t, char_t * const >,
-        is_detected<char_ptr_t, char_t * &&>,
-        is_detected<char_ptr_t, char_t * &>,
-        is_detected<char_ptr_t, char_t * >,
-        negation< is_detected<char_ptr_t, char_t  const &&> >,
-        negation< is_detected<char_ptr_t, char_t  const &> >,
-        negation< is_detected<char_ptr_t, char_t  const > >,
-        negation< is_detected<char_ptr_t, char_t  &&> >,
-        negation< is_detected<char_ptr_t, char_t  &> >,
-        negation< is_detected<char_ptr_t, char_t  > >
-    >;
-
-    template <
-        typename char_t,
-        typename fail = std::false_type
-    >
-    constexpr auto char_ptr_t_detected_v = char_ptr_t_detected<char_t, fail>::value;
-
-    static_assert__(( char_ptr_t_detected_v<char> ));
-    static_assert__(( char_ptr_t_detected_v<signed char> ));
-    static_assert__(( !char_ptr_t_detected_v<unsigned char> ));
-    static_assert__(( char_ptr_t_detected_v<wchar_t> ));
-    static_assert__(( char_ptr_t_detected_v<char8_t> ));
-    static_assert__(( char_ptr_t_detected_v<char16_t> ));
-    static_assert__(( char_ptr_t_detected_v<char32_t> ));
-    static_assert__(( !char_ptr_t_detected_v<short> ));
-    static_assert__(( !char_ptr_t_detected_v<int> ));
-    static_assert__(( !char_ptr_t_detected_v<long> ));
-    static_assert__(( !char_ptr_t_detected_v<unsigned short> ));
-    static_assert__(( !char_ptr_t_detected_v<unsigned long> ));
-
-    //
-    // view
-    //
-    constexpr char const* lol = "lol";
-    constexpr std::array<char, 4> lol1 { "lol" };
-
-    static_assert__(( view(lol).size() == 3 ));
-    static_assert__(( view(lol1).size() == 4 ));
-
-    constexpr auto src0 = std::array<char, 12>{"hi bob"};
-    constexpr auto src1 = u::view::view { src0 };
-    constexpr auto v0 = u::str::view(src0);
-    constexpr auto v1 = u::str::view(src1);
-    static_assert(v0.size() == src0.size());
-    static_assert(v1.size() == src1.size());
-
-    //
-    // cmp
-    //
-    static_assert__(( cmp(lol, lol1) == 0 ));
-    static_assert__(( cmp(lol1, lol) == 0 ));
-    static_assert__(( cmp(src0, v0) == 0 ));
-    static_assert__(( cmp(src0, src1) == 0 ));
-    static_assert__(( cmp(v0, v1) == 0 ));
-    static_assert__(( cmp(v0, lol1) != 0 ));
-
-    //
-    // copy
-    //
-    static_assert__(( cmp("hi", copy("hi", std::array<char, 5>{}.data())) == 0 ));
+//    using stdx::is_detected;
+//    using std::negation;
+//
+//     //
+//     // char_ptr_t
+//     //
+//     template <
+//         typename char_t = char,
+//         typename fail = std::false_type
+//     >
+//     using char_ptr_t_detected = std::conjunction<
+//         negation<fail>,
+//         is_detected<char_ptr_t, char_t const* const &&>,
+//         is_detected<char_ptr_t, char_t const* const &>,
+//         is_detected<char_ptr_t, char_t const* const >,
+//         is_detected<char_ptr_t, char_t const* &&>,
+//         is_detected<char_ptr_t, char_t const* &>,
+//         is_detected<char_ptr_t, char_t const* >,
+//         is_detected<char_ptr_t, char_t * const &&>,
+//         is_detected<char_ptr_t, char_t * const &>,
+//         is_detected<char_ptr_t, char_t * const >,
+//         is_detected<char_ptr_t, char_t * &&>,
+//         is_detected<char_ptr_t, char_t * &>,
+//         is_detected<char_ptr_t, char_t * >,
+//         negation< is_detected<char_ptr_t, char_t  const &&> >,
+//         negation< is_detected<char_ptr_t, char_t  const &> >,
+//         negation< is_detected<char_ptr_t, char_t  const > >,
+//         negation< is_detected<char_ptr_t, char_t  &&> >,
+//         negation< is_detected<char_ptr_t, char_t  &> >,
+//         negation< is_detected<char_ptr_t, char_t  > >
+//     >;
+//
+//     template <
+//         typename char_t,
+//         typename fail = std::false_type
+//     >
+//     constexpr auto char_ptr_t_detected_v = char_ptr_t_detected<char_t, fail>::value;
+//
+//     static_assert__(( char_ptr_t_detected_v<char> ));
+//     static_assert__(( char_ptr_t_detected_v<signed char> ));
+//     static_assert__(( !char_ptr_t_detected_v<unsigned char> ));
+//     static_assert__(( char_ptr_t_detected_v<wchar_t> ));
+//     static_assert__(( char_ptr_t_detected_v<char8_t> ));
+//     static_assert__(( char_ptr_t_detected_v<char16_t> ));
+//     static_assert__(( char_ptr_t_detected_v<char32_t> ));
+//     static_assert__(( !char_ptr_t_detected_v<short> ));
+//     static_assert__(( !char_ptr_t_detected_v<int> ));
+//     static_assert__(( !char_ptr_t_detected_v<long> ));
+//     static_assert__(( !char_ptr_t_detected_v<unsigned short> ));
+//     static_assert__(( !char_ptr_t_detected_v<unsigned long> ));
+//
+//     //
+//     // view
+//     //
+//     constexpr char const* lol = "lol";
+//     constexpr std::array<char, 4> lol1 { "lol" };
+//
+//     static_assert__(( view(lol).size() == 3 ));
+//     static_assert__(( view(lol1).size() == 4 ));
+//
+//     constexpr auto src0 = std::array<char, 12>{"hi bob"};
+//     constexpr auto src1 = u::view::view { src0 };
+//     constexpr auto v0 = u::str::view(src0);
+//     constexpr auto v1 = u::str::view(src1);
+//     static_assert(v0.size() == src0.size());
+//     static_assert(v1.size() == src1.size());
+//
+//     //
+//     // cmp
+//     //
+//     static_assert__(( cmp(lol, lol1) == 0 ));
+//     static_assert__(( cmp(lol1, lol) == 0 ));
+//     static_assert__(( cmp(src0, v0) == 0 ));
+//     static_assert__(( cmp(src0, src1) == 0 ));
+//     static_assert__(( cmp(v0, v1) == 0 ));
+//     static_assert__(( cmp(v0, lol1) != 0 ));
+//
+//     //
+//     // copy
+//     //
+//     static_assert__(( cmp("hi", copy("hi", std::array<char, 5>{}.data())) == 0 ));
 }
 namespace u::spec::str
 {
--- src/u/str.h
+++ src/u/str.h
@@ -8,55 +8,6 @@
 #include <cstring>
 #include <algorithm>
 #include <type_traits>
-namespace u::str
-{
-    //! The underlying char type after deptr-ing a type
-    template <
-        typename T,
-        typename = std::enable_if_t<
-            std::conjunction_v<
-                std::is_pointer<stdx::remove_cvref_t<T>>,
-                util::is_char<util::deptr_t<T>>
-            >,
-            void
-        >
-    >
-    using char_ptr_t = util::deptr_t<T>;
-
-    //! Char traits for a type after deptr-ing it
-    template <
-        typename T
-    >
-    using char_traits = std::char_traits<char_ptr_t<T>>;
-}
-
-//
-//! A char* is also an iterable!
-template <
-    typename CharPtr,
-    std::enable_if_t<
-        stdx::is_detected_v<u::str::char_ptr_t, CharPtr>,
-        int
-    > = 0
->
-constexpr stdx::remove_cvref_t<CharPtr> begin(CharPtr&& cstr)
-{
-    return cstr;
-}
-
-template <
-    typename CharPtr,
-    std::enable_if_t<
-        stdx::is_detected_v<u::str::char_ptr_t, CharPtr>,
-        int
-    > = 0
->
-//! A char* is also an iterable!
-constexpr stdx::remove_cvref_t<CharPtr> end(CharPtr&& cstr)
-{
-    return cstr + std::char_traits<u::str::char_ptr_t<CharPtr>>::length(cstr);
-}
-
 namespace std
 {
     //! Start_with seems to be sometimes missing
@@ -67,56 +18,5 @@ namespace std
             prefix.data(),
             prefix.size()
         ) == 0;
-    }
-}
-
-namespace u::str
-{
-    template <
-        typename View
-    >
-    constexpr std::string_view view(View const& view)
-    {
-        const auto first = begin(view);
-        const auto last = end(view);
-        const std::size_t size = std::distance(first, last);
-        return { first, size };
-    }
-
-    template <
-        typename T
-    >
-    using view_t = decltype(view(std::declval<T>()));
-
-
-    //! Compare any to any
-    template <
-        typename A,
-        typename B
-    >
-    constexpr auto cmp(A const& a, B const& b)
-    {
-        const auto va = view(a);
-        const auto vb = view(b);
-        const auto astr = va.data();
-        const auto bstr = vb.data();
-        const auto n = std::min(va.size(), vb.size());
-        using ct = char_traits<decltype(astr)>;
-        return ct::compare(astr, bstr, n);
-    }
-
-
-    //! Copy any to any
-    template <
-        typename Src,
-        typename Dest
-    >
-    constexpr auto copy(Src const& src, Dest&& dest)
-    {
-        const auto vsrc = view(util::fwd(src));
-        const auto srcstr = vsrc.data();
-        using ct = char_traits<decltype(dest)>;
-        const auto n = std::min(vsrc.size(), ct::length(dest));
-        return ct::copy(dest, srcstr, n);
     }
 }
